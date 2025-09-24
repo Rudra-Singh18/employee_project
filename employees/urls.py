b22b1fde908907b26209071145a8f50c5772badb
Rudra-Singh18 <rudrasingh0094@gmail.com>
2025-09-24 11:13:50 +0530

    Updated Employee commit

--- employees/urls.py
+++ employees/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import DepartmentViewSet, EmployeeViewSet
 
+
 router = DefaultRouter()
 router.register(r'departments', DepartmentViewSet, basename='department')
 router.register(r'employees', EmployeeViewSet, basename='employee')
